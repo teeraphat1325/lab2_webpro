1654bf59a1dfa35679829fcd7852739a91fc2240
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 19:28:34 +0700

    try v-bind

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -3,6 +3,14 @@ var msg = 'Hello Message'
 var rawHtml = '<h1>Hello</h1>'
 var myId = 'myId1'
 var color = 'blue'
+var isDisable = true
+var objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper',
+  style: 'background-color:green'
+}
+var number = 20
+var ok = false
 </script>
 
 <template>
@@ -10,4 +18,8 @@ var color = 'blue'
     <div v-html="rawHtml"></div>
     <div v-bind:id="myId"></div>
     <font :color="color">Red</font>
+    <button :disabled="isDisable">isEnable</button>
+    <div v-bind="objectOfAttrs">Background Green</div>
+    <div>{{ number +1 }}</div>
+    <div>{{ ok ? 'Yes':'No' }}</div>
 </template>
