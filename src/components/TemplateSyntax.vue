eca82e8a0a5d6e7765a5bdf1ee742dcff54ff7b0
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 19:16:14 +0700

    try v-html and v-bind

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,3 +1,13 @@
+<script setup>
+var msg = 'Hello Message'
+var rawHtml = '<h1>Hello</h1>'
+var myId = 'myId1'
+var color = 'blue'
+</script>
+
 <template>
-    <div>Template Syntax</div>
+    <div>{{ msg }}</div>
+    <div v-html="rawHtml"></div>
+    <div v-bind:id="myId"></div>
+    <font :color="color">Red</font>
 </template>
