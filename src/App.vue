1e26fa42011eeebde1166818579326c018062bab
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 18:56:01 +0700

    Add import component and test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-
+import Hello from './components/HelloComponent.vue'
 </script>
 
 <template>
   <div>Hello</div>
+  <span><Hello></Hello></span>
 </template>
 
 <style scoped>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Hello</div>
+</template>
+
+<style scoped>
+div {
+    font-size: 25pt;
+}
+</style>
