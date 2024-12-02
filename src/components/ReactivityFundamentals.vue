f2d2756b4fd346aa9ef79c8cbc66a663b827128a
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 19:47:06 +0700

    Understanding ref and reactive

--- src/components/ReactivityFundamentals.vue
+++ src/components/ReactivityFundamentals.vue
@@ -1,11 +1,20 @@
 <template>
     <div>{{ count }}</div>
     <button @click="increment()">{{ count }}</button>
+
+    <div>{{ state.count }}</div>
+    <button @click="increment1()">{{ state.count }}</button>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref , reactive } from 'vue'
+const state = reactive({
+    count: 0,
 
+})
+function increment1() {
+    state.count++
+}
 const count = ref(0)
 function increment() {
     count.value++
