41fa4d7e2a2578aeb519273af07af96dcf5177cd
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 20:07:38 +0700

    Function and arrow Function

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
 import ReactiveFundamentals from './components/ReactivityFundamentals.vue'
+import FormInputBindings from './components/FormInputBindings.vue'
 </script>
 
 <template>
-
+  <FormInputBindings></FormInputBindings>
+  <hr>
   <ReactiveFundamentals></ReactiveFundamentals>
   <hr>
   <div>Hello</div>
---
+++ src/components/FormInputBindings.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Form Input Bindings</div>
+    <div>{{ text }}</div>
+    <input type="text" :value="text" @input="(e) => (text = e.target.value)"/>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text = ref('hello')
+// const inputText =  (e) => (text.value = e.target.value)
+</script>
