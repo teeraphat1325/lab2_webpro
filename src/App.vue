7c0eeb9cc6d0afe895be37e2270afd789c0cb649
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 19:38:54 +0700

    Add ReactivityFundamentals component and try export default

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
+import ReactiveFundamentals from './components/ReactivityFundamentals.vue'
 </script>
 
 <template>
+
+  <ReactiveFundamentals></ReactiveFundamentals>
+  <hr>
   <div>Hello</div>
+  <hr>
   <span><Hello></Hello></span>
+  <hr>
   <TemplateSyntax></TemplateSyntax>
+  <hr>
 </template>
 
 <style scoped>
---
+++ src/components/ReactivityFundamentals.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{ count }}</div>
+    <button @click="increment()">{{ count }}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+function increment() {
+    count.value++
+}
+</script>
