34bdd7ce4f1959b835c351eb5a14c9f595fd079d
Teeraphat Prannok <66160068@go.buu.ac.th>
2024-12-02 19:02:19 +0700

    Add TemplateSyntax component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Hello from './components/HelloComponent.vue'
+import TemplateSyntax from './components/TemplateSyntax.vue'
 </script>
 
 <template>
   <div>Hello</div>
   <span><Hello></Hello></span>
+  <TemplateSyntax></TemplateSyntax>
 </template>
 
 <style scoped>
---
+++ src/components/TemplateSyntax.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Template Syntax</div>
+</template>
